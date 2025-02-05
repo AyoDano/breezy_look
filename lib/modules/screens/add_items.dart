feae5ecda25d813e5d6b38da1c4bdf404a90e25c
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-02-05 08:57:00 +0100

    feat(add-items): add more selection options, input field, and convert list to SliverList

--- lib/modules/screens/add_items.dart
+++ lib/modules/screens/add_items.dart
@@ -1,4 +1,8 @@
+import 'package:breezy_look/config/themes/theme_light.dart';
+import 'package:breezy_look/widgets/button_no_icon_widget.dart';
 import 'package:breezy_look/widgets/selectable_chips_row.dart';
+import 'package:breezy_look/widgets/size_selector.dart';
+import 'package:breezy_look/widgets/slider_widget.dart';
 import 'package:breezy_look/widgets/textfield_input.dart';
 import 'package:flutter/material.dart';
 
@@ -8,52 +12,107 @@ class AddItemsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // No back chevron in appbar
-        actions: [
-          IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              Navigator.pop(
-                  context); // Closes the page and back to starting point
-            },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 120,
+            floating: false,
+            pinned: true,
+            backgroundColor: AppTheme.backgroundColor,
+            elevation: 4,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                "Add New Item",
+                style: AppTheme.lightTheme.textTheme.displaySmall,
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.asset(
-                  "assets/fashion_items/longsleeve_striped_red_wht.png",
-                  width: 200,
-                  height: 280,
-                  fit: BoxFit.cover,
+              background: Container(color: AppTheme.backgroundColor),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          "assets/fashion_items/longsleeve_striped_red_wht.png",
+                          width: 200,
+                          height: 280,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFieldWithLabel(
+                        label: "Item Name: *",
+                        placeholder: "Black Cardigan, Pants..."),
+                    const SizedBox(height: 30),
+                    SelectableChipsRow(
+                      label: 'Category:',
+                      chipOptions: ["Shirt", "Pants", "Accessorie", "Shoes"],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Divider(color: Colors.grey.shade300),
+                    ),
+                    SelectableChipsRow(
+                      label: 'Style:',
+                      chipOptions: ["Casual", "Formal", "Sport", "Elegant"],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Divider(color: Colors.grey.shade300),
+                    ),
+                    SizeSelectorRow(label: "Size", chipOptions: [
+                      "XS",
+                      "S",
+                      "M",
+                      "L",
+                      "XL",
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Divider(color: Colors.grey.shade300),
+                    ),
+                    SelectableChipsRow(
+                      label: 'Material:',
+                      chipOptions: ["Wool", "Leather", "Cotton", "Silk"],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Divider(color: Colors.grey.shade300),
+                    ),
+                    SelectableChipsRow(
+                      label: 'Finishing / Feeling:',
+                      chipOptions: [
+                        "Waterproof",
+                        "Breatheable",
+                        "Cotton",
+                        "Silk"
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Divider(color: Colors.grey.shade300),
+                    ),
+                    SliderWidget(label: "Sweat"),
+                    const SizedBox(height: 20),
+                    IconlessButtonWidget(text: "Add Item", onPressed: () {}),
+                    const SizedBox(height: 20),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextFieldWithLabel(
-                label: "Item Name: *", placeholder: "Black Cardigan, Pants..."),
-            const SizedBox(height: 20),
-            SelectableChipsRow(
-              label: 'Category: *',
-              chipOptions: ["Shirt", "Pants", "Accessorie", "Shoes"],
-            ),
-            Divider(),
-            SelectableChipsRow(
-              label: 'Style: *',
-              chipOptions: ["Casual", "Formal", "Sport", "Elegant"],
-            ),
-          ],
-        ),
+            ]),
+          ),
+        ],
       ),
     );
   }
