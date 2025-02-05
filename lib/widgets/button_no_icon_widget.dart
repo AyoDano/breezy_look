74206d2f88c16054157b583a48d995285e140893
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-02-05 08:59:29 +0100

    feat(button): create custom button widget without icon

---
+++ lib/widgets/button_no_icon_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:breezy_look/config/themes/theme_light.dart';
+
+class IconlessButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+
+  const IconlessButtonWidget({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 70),
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          decoration: BoxDecoration(
+            color: AppTheme.primaryColor,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: AppTheme.primaryColor.withValues(alpha: 0.2),
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: AppTheme.backgroundColor,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
