8db5013434208b6d545b8075c90529bfc522decf
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-02-07 09:08:44 +0100

    feat: extracting MyApp from Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
-import 'package:breezy_look/modules/screens/add_items.dart';
-import 'package:breezy_look/modules/screens/home_screen.dart';
+import 'package:breezy_look/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:breezy_look/config/themes/theme_light.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BreezyLook Theme',
-      theme: AppTheme.lightTheme, // This is where I use my custom theme
-      home: HomeScreen(), // Here is where the App starts / Starting point
-      routes: {
-        '/add_items': (context) => AddItemsScreen(),
-      },
-    );
-  }
 }
