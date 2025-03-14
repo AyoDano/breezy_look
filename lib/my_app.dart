cd4ab308ff90b996ad133cc195909f31e4eb528e
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-03-14 16:10:21 +0100

    chore: update app name and change entrypoint screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,9 +1,9 @@
 import 'package:breezy_look/config/themes/theme_light.dart';
-import 'package:breezy_look/modules/screens/login_screen.dart';
+import 'package:breezy_look/modules/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BreezyLookApp extends StatelessWidget {
+  const BreezyLookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'BreezyLook',
       theme: AppTheme.lightTheme, // Custom theme of my application
-      home: LoginScreen(""), // Starting point
+      home: HomeScreen(),
+      // LoginScreen(databaseRepository: MockDatabase()),
+      // Starting point
     );
   }
 }
