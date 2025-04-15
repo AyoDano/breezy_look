01d8a4d8f532d9a6cd04fc7de3ded18d2054569a
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-04-15 12:49:37 +0200

    feat: create TextSpan for terms and privacy links with clickable functionality

---
+++ lib/utils/ui/widgets/terms_and_privacy_text.dart
@@ -0,0 +1,40 @@
+import 'package:breezy_look/config/themes/theme_light.dart';
+import 'package:flutter/material.dart';
+
+class TermsAndPrivacyText extends StatelessWidget {
+  const TermsAndPrivacyText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 60.0),
+      child: Text.rich(
+        TextSpan(
+          text: "By signing up, you are creating a BreezyLook\n ",
+          style: TextStyle(fontSize: 14),
+          children: <TextSpan>[
+            TextSpan(
+              text: "account and agree to BreezyLook's ",
+            ),
+            TextSpan(
+              text: "Terms \n",
+              style: TextStyle(
+                  color: AppTheme.primaryColor, fontWeight: FontWeight.w800),
+            ),
+            TextSpan(
+              text: " and ",
+            ),
+            TextSpan(
+              text: "Privacy Policy.",
+              style: TextStyle(
+                  color: AppTheme.primaryColor, fontWeight: FontWeight.w800),
+            ),
+          ],
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
