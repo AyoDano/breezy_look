7b508a2659d8979df8a52de4f3976501e2125ae8
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-04-15 12:24:51 +0200

    chore: initialize Firebase and set up MultiProvider with NavigationProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
-import 'package:breezy_look/my_app.dart';
+import 'package:breezy_look/modules/data/navigation_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(BreezyLookApp());
+import 'package:provider/provider.dart';
+import 'package:breezy_look/my_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NavigationProvider()),
+      ],
+      child: const BreezyLookApp(),
+    ),
+  );
 }
