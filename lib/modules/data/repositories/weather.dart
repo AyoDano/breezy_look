53f93f21fedc703ac3f9174afaab268cecae1fe2
AyoDano <100164168+AyoDano@users.noreply.github.com>
2025-05-05 12:13:14 +0200

    feat: add simple Weather class with display and update methods
    Added a basic Weather class with temperature, condition, and location. Now able show or change the weather info.
    
    This will be just a placeholder – real weather with location will be added later using a weather API and geolocator.

---
+++ lib/modules/data/repositories/weather.dart
@@ -0,0 +1,26 @@
+// Zu erst erstelle ich in dieser File meine Klasse Weather
+// mit den passenden Attributren
+class Weather {
+  double temperature;
+  String condition;
+  String location;
+
+// Nun Folgt der Konstruktor, welcher die Attribute der Klasse beinhaltet
+// dieser ist Bestandteil für initialisierte Objekte vom Weather.
+  Weather(this.temperature, this.condition, this.location);
+
+// Die Methode displayWeather dient zum Anzeigen der Wetterinformationen
+// es wirde Ausgegeben, was in der Klammer nach print kommt. Mit Hilfe
+// von string interpolationen
+  void displayWeather() {
+    print("The weather $location is $condition and has: $temperature°C.");
+  }
+
+// Eine weitere Methode, um eventuelle wetterupdates auszugeben
+// und die alten zu überschreiben, ohne dass es @override bedarf
+// die neuen Parameter werden den alten Instanzen zugewiesen
+  void updateWeather(String newCondition, double newTemperature) {
+    condition = newCondition;
+    temperature = newTemperature;
+  }
+}
